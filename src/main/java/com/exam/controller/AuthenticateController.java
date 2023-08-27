9d920c60ea24a6326ccb90cb7bec3a2ab7216c55
codewithabhi <abhishekgarg094@gmail.com>
2023-08-27 08:50:30 +0530

    login issue resolve

--- src/main/java/com/exam/controller/AuthenticateController.java
+++ src/main/java/com/exam/controller/AuthenticateController.java
@@ -3,11 +3,15 @@ package com.exam.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,30 +37,43 @@ public class AuthenticateController {
 	private JwtUtils jwtUtils;
 	
 	@PostMapping("/generate-token")
-	public ResponseEntity<?> generateToken(@RequestBody JWTRequest request)
+	public ResponseEntity<?> generateToken(@RequestBody JWTRequest request) throws Exception
 	{
+		String token="";
 		
 		try {
 			Authenticate(request.getUsername(), request.getPassword());
-		} catch (Exception e) {
+			
+			
+		} catch (UsernameNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("exception in Auth controller");
 			e.printStackTrace();
+			
+			throw new Exception("User not found"+e.getMessage());
 		}
 		UserDetails userDetails= userDetailservice.loadUserByUsername(request.getUsername());
-		String token=jwtUtils.generateToken(userDetails);
+		 token=jwtUtils.generateToken(userDetails);
 		return ResponseEntity.ok(new JWTResponse(token));
+		
 	}
 	
-	public void Authenticate(String username, String password)
+	public void Authenticate(String username, String password) throws Exception
 	{
 		
 		try {
 			System.out.println("Auth controller try block");
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (AuthenticationException e) {
+		} 
+		 catch (DisabledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new Exception("User Disabled" +e.getMessage());
+			}
+		catch (BadCredentialsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new UsernameNotFoundException("Invalid Credentials" +e.getMessage());
 		}
 	}
 	@GetMapping("/current-user")
